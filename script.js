fff61ac41376bae2a68fbc749f3820bc7d508ca3
Kyle <notxap55@yahoo.com>
2022-10-12 15:57:34 -0700

    basic styling

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+let myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary() {
+
+
+  myLibrary.push(getBook)
+}
