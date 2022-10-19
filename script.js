e1bbca4299428f82847261e612b31871dd5107c3
Kyle <notxap55@yahoo.com>
2022-10-19 15:05:00 -0700

    modal implemented

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const addBookBtn = document.querySelector('#add-btn');
+const modal = document.querySelector('.modal');
+const modalBg = document.querySelector('.modal-bg')
+
+addBookBtn.addEventListener('click', () => {
+  modal.classList.add('unhide');
+  modalBg.classList.add('unhide');
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target === modalBg) {
+    modal.classList.remove('unhide');
+    modalBg.classList.remove('unhide');
+  }
+})
+
+
+
+
+
+
+
 let myLibrary = [];
 
 
